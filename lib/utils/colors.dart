9317b47591da6399bc5dad0ca799fcb4d82dcafe
Wandile Kubheka <retroff01@gmail.com>
2025-04-24 19:50:11 +0200

    default theming and fonts

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class CColors {
+  static Color lightTextColor = HexColor("#FFFFF");
+  static Color darkTextColor = HexColor("#444E72");
+  static Color lightGreyText = HexColor("#838BAA");
+  // static  Color backgroundGradientColor = Color.
+  static Color lightBlue = HexColor("#47BFDF");
+  static Color darkBlue = HexColor("#4A91FF");
+}
--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/utils/colors.dart';
 
 class Ctheme {
-  static ThemeData cThemedata = ThemeData();
+  static ThemeData cThemedata = ThemeData(
+    textTheme: TextTheme(
+      bodyMedium: TextStyle(
+        color: CColors.lightTextColor,
+        fontSize: 12,
+        fontFamily: "Overpass",
+      ),
+      titleLarge: TextStyle(
+        color: CColors.lightTextColor,
+        fontWeight: FontWeight.bold,
+        fontFamily: "Overpass",
+        fontSize: 70,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 14,
+        fontFamily: "Overpass",
+        fontWeight: FontWeight.bold,
+        color: CColors.lightTextColor,
+      ),
+    ),
+  );
 }
