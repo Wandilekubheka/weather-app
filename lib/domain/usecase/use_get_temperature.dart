c2856fd6194956211fa4e993853ae2db2c8238bf
Wandile Kubheka <retroff01@gmail.com>
2025-04-28 00:22:29 +0200

    get temperation usecase

--- lib/domain/usecase/use_get_temperature.dart
+++ lib/domain/usecase/use_get_temperature.dart
@@ -1,18 +1,25 @@
 import 'package:weather_app/data/models/position.dart';
+import 'package:weather_app/data/models/weather_model.dart';
 import 'package:weather_app/domain/repository/weather_repo.dart';
 
 class UseGetTemperature {
   final WeatherRepository _weatherRepository;
   final Position _position;
 
+  const UseGetTemperature(
+    this._position, {
+    required WeatherRepository weatherRepository,
+  }) : _weatherRepository = weatherRepository;
 
-  const UseGetTemperature(this._position, {required WeatherRepository weatherRepository})
-    : _weatherRepository = weatherRepository;
-
-
-    Future<int> getTemperature()async{
-      try{
-        await _weatherRepository.getWeatherData(long, lat)
-      }
+  Future<double> getTemperature() async {
+    try {
+      WeatherResponse response = await _weatherRepository.getWeatherData(
+        _position,
+      );
+      return response.current.temp;
+    } catch (e) {
+      // render prev temps from datebase
+      rethrow;
     }
+  }
 }
